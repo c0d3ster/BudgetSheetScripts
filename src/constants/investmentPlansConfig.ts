d5888167a48610874060f4885250de487623db4f
cody douglass <c0d3ster@vt.edu>
2025-07-10 17:58:50 -0400

    fix: baller description

--- src/constants/investmentPlansConfig.ts
+++ src/constants/investmentPlansConfig.ts
@@ -44,7 +44,7 @@ export const INVESTMENT_PLANS_CONFIG = {
       'Education Fund': 0,
       tagline: 'For those who want to ball out!',
       description:
-        'Luxury-focused strategy emphasizing lifestyle and experiences. Heavy investment in vacation funding and precious metals to live your best life in the here and now..',
+        'Luxury-focused strategy emphasizing lifestyle and experiences. Heavy investment in vacation funding and precious metals to live your best life in the here and now.',
     },
   },
 
